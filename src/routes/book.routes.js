eaa823324703c33e69f110e7a56caf2cc5c78986
narendran-dev <narendrankdev@gmail.com>
2025-09-25 17:42:32 +0530

    added validations for books

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,10 +1,12 @@
-const contoller = require('../controller/controller.book.js')
+const contoller = require('../controller/controller.book.js');
+const { bookValidation } = require('../util/bookValidator.js');
+const { validator } = require('../middleware/validators.js')
 module.exports = app => {
     const route = require('express').Router();
     const bookPath = '/books'
-    route.post(bookPath, contoller.addbook); 
-    route.get(bookPath,contoller.findAllBooks);
-    route.delete(`${bookPath}/:id`,contoller.deleteABook)
-    route.put(`${bookPath}/:id`,contoller.updateBook)
+    route.post(bookPath,bookValidation,validator, contoller.addbook);
+    route.get(bookPath, contoller.findAllBooks);
+    route.delete(`${bookPath}/:id`, contoller.deleteABook)
+    route.put(`${bookPath}/:id`, contoller.updateBook)
     app.use('/api', route);
 };
---
+++ src/util/bookValidator.js
@@ -0,0 +1,26 @@
+const { body } = require('express-validator');
+const db = require('../model');
+
+const Book = db.Book;
+const allowedGenres = ['Action & Adventure', 'Mystery', 'Fantasy', 'Science Fiction', 'Romance', 'Historical Fiction'];
+const date = new Date();
+const year = date.getFullYear();
+
+exports.bookValidation = [
+    body('title').trim().notEmpty().withMessage('Book title cant be empty').isLength({ max: 1000 }).withMessage('Title cannot be longer than 1000 characters').custom(async title => {
+        let finder = title.trim();
+        try {
+            const existing = await Book.findOne({ title: finder }).collation({ locale: 'en', strength: 2 }).exec();
+            if (existing) {
+                throw new Error('Title already exists!');
+            }
+        } catch (err) {
+            throw err;
+        }
+    }),
+    body('author').trim().notEmpty().withMessage('author cant be empty').isLength({ max: 1000 })
+        .withMessage('Title cannot be longer than 1000 characters'), ,
+    body('genre').trim().notEmpty().withMessage('gener cant be empty').isIn(allowedGenres).withMessage(`Genre must be one of: ${allowedGenres.join(', ')} `),
+    body('year').trim().notEmpty().withMessage('year cant be empty').isFloat({ max: year }).withMessage(`year cant be greater than current year -> ${year}`)
+]
+
