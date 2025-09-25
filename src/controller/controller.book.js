0e220693745dd86aeaf3d1a11392632e970c5c4b
narendran-dev <narendrankdev@gmail.com>
2025-09-25 15:43:47 +0530

    added update and delete books

--- src/controller/controller.book.js
+++ src/controller/controller.book.js
@@ -1,6 +1,6 @@
 const db = require('../model');
 const Book = db.Book;
-exports.add = async (req, res) => {
+exports.addbook = async (req, res) => {
     try {
         const book = new Book({
             title: req.body.title,
@@ -13,7 +13,66 @@ exports.add = async (req, res) => {
         res.send(data)
     } catch (err) {
         res.status(400).send({
-            message: err.message || "Error occurred while creating the Tutorial."
+            message: err.message || "Error occurred while creating the book."
         });
     }
 }
+exports.findAllBooks = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+
+    Book.find(condition)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving books."
+            });
+        });
+};
+exports.deleteABook = (req,res) =>{
+const id = req.params.id;
+
+  Book.findByIdAndDelete(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Book with id=${id}. Maybe Book was not found!`
+        });
+      } else {
+        res.send({
+          message: "Book was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete book with id=" + id
+      });
+    });
+}
+exports.updateBook = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  Book.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Book with id=${id}. Maybe Book was not found!`
+        });
+      } else res.send({ message: "Book was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Book with id=" + id
+      });
+    });
+};
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,6 +1,10 @@
 const contoller = require('../controller/controller.book.js')
 module.exports = app => {
     const route = require('express').Router();
-    route.post('/books', contoller.add); 
+    const bookPath = '/books'
+    route.post(bookPath, contoller.addbook); 
+    route.get(bookPath,contoller.findAllBooks);
+    route.delete(`${bookPath}/:id`,contoller.deleteABook)
+    route.put(`${bookPath}/:id`,contoller.updateBook)
     app.use('/api', route);
 };
