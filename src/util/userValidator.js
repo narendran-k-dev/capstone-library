f2f022dcbb457abb9ae5f9c2443fffefbcb20c54
narendran-dev <narendrankdev@gmail.com>
2025-10-03 18:28:47 +0530

    moved password policy to env

--- src/util/userValidator.js
+++ src/util/userValidator.js
@@ -1,8 +1,10 @@
 
 const { body } = require('express-validator');
+const policy = new RegExp(process.env.PASSWORD_POLICY)
+const pswderror = process.env.PASSWORD_POLICY_MSG;
 exports.userValidator = [
     body('username').trim().notEmpty().withMessage('username cant be empty').isString('username must be a string ').matches(/^[a-zA-Z0-9_]+$/).withMessage('Username can only contain letters, numbers, and underscores'),
     body('email').trim().notEmpty().withMessage('email cant be empty').isEmail().withMessage('please follow standard email formats').normalizeEmail(),
-    body('password').trim().notEmpty().withMessage('password cant be empty').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/)
-        .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'),
+    body('password').trim().notEmpty().withMessage('password cant be empty').matches(policy)
+        .withMessage(pswderror),
 ]
