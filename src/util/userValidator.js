533d2222414b7a6f3b9538bafcab2e5bf6e346f9
narendran-dev <narendrankdev@gmail.com>
2025-10-03 21:06:41 +0530

    updating password policy

--- src/util/userValidator.js
+++ src/util/userValidator.js
@@ -1,10 +1,30 @@
-
 const { body } = require('express-validator');
-const policy = new RegExp(process.env.PASSWORD_POLICY)
-const pswderror = process.env.PASSWORD_POLICY_MSG;
+
+const lowercase = new RegExp(process.env.PASSWORD_LOWERCASE);
+const uppercase = new RegExp(process.env.PASSWORD_UPPERCASE);
+const number = new RegExp(process.env.PASSWORD_NUMBER);
+const special = new RegExp(process.env.PASSWORD_SPECIAL);
+const minLength = parseInt(process.env.PASSWORD_MIN_LENGTH, 10);
+
 exports.userValidator = [
-    body('username').trim().notEmpty().withMessage('username cant be empty').isString('username must be a string ').matches(/^[a-zA-Z0-9_]+$/).withMessage('Username can only contain letters, numbers, and underscores'),
-    body('email').trim().notEmpty().withMessage('email cant be empty').isEmail().withMessage('please follow standard email formats').normalizeEmail(),
-    body('password').trim().notEmpty().withMessage('password cant be empty').matches(policy)
-        .withMessage(pswderror),
-]
+    body('username')
+        .trim()
+        .notEmpty().withMessage('Username can’t be empty')
+        .isString().withMessage('Username must be a string')
+        .matches(/^[a-zA-Z0-9_]+$/).withMessage('Username can only contain letters, numbers, and underscores'),
+
+    body('email')
+        .trim()
+        .notEmpty().withMessage('Email can’t be empty')
+        .isEmail().withMessage('Please follow standard email formats')
+        .normalizeEmail(),
+
+    body('password')
+        .trim()
+        .notEmpty().withMessage('Password can’t be empty')
+        .isLength({ min: minLength }).withMessage(process.env.PASSWORD_ERROR_MINLENGTH)
+        .matches(lowercase).withMessage(process.env.PASSWORD_ERROR_LOWERCASE)
+        .matches(uppercase).withMessage(process.env.PASSWORD_ERROR_UPPERCASE)
+        .matches(number).withMessage(process.env.PASSWORD_ERROR_NUMBER)
+        .matches(special).withMessage(process.env.PASSWORD_ERROR_SPECIAL),
+];
